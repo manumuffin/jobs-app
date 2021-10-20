4b5876f5f1fd84bb10d23e2c79f35b1fe37c1b16
Manuela <manuelagreimel@yahoo.de>
2021-10-20 11:24:04 +0200

    Second session; skeleton done

--- github-jobs/src/App.js
+++ github-jobs/src/App.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useFetchJobs from './useFetchJobs';
 import { Container } from 'react-bootstrap';
+import Job from './Job';
+import JobsPagination from './JobsPagination';
+import SearchForm from './SearchForm';
 
 function App() {
+  const [params, setParams] = useState({});
+  const [page, setPage] = useState(1);
+  const {jobs, loading, error, hasNextPage} = useFetchJobs(params, page);
 
-  const {jobs, loading, error} = useFetchJobs();
+  function handleParamChange(e) {
+    const param = e.target.name
+    const value = e.target.value
+    setPage(1) //go back to page 1 when searching
+    setParams(prevParams => {
+      return { ...prevParams, [param]: value }
+    })
+  }
 
   return (
-      <Container>
+      <Container className="my-4">
+        <h1 className="mb-4">GitHub Jobs</h1>
+        <SearchForm params={params} onParamChange={handleParamChange} />
+        <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage}/>
         {loading && <h1>Loading...</h1>}
         {error && <h1>Error. Try refreshing.</h1>}
-        <h1>{jobs.length}</h1>
+        {jobs.map(job => { // print out each job
+          return <Job key={job.id} job={job}></Job> //needs key because it's in a loop & proper rendering
+        })}
+        <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage}/>
       </Container>
   );
 }
